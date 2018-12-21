b763d646cd4f30bb9661165d70eacdf2b53c2c1c
Truong Hoang Phuong <thp.1093@outlook.com>
2018-12-21 15:19:38 +0700

    Add caddy image

---
+++ caddy/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+write_configs() {
+    for f in $(find $1 -regex ".*$2\$"); do
+        envsubst < $f > "$3/$(basename $f)"
+    done
+}
+
+write_configs /tmp/php-conf .ini /usr/local/etc/php/conf.d
+write_configs /tmp/php-fpm .conf /usr/local/etc/php-fpm.d
+
+exec /bin/parent caddy "$@"
