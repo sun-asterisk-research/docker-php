85b6efc78458308219b0646d300a46f043629912
Truong Hoang Phuong <thphuong1093@gmail.com>
2022-03-02 10:19:42 +0700

    fix: properly configure nginx on both distros

--- nginx/etc/cont-init.d/10-nginx-conf-envsubst.sh
+++ nginx/etc/cont-init.d/10-nginx-conf-envsubst.sh
@@ -2,6 +2,8 @@
 
 vars=$(env | cut -d= -f1 | awk '{print "${" $1 "}"}')
 
+mkdir -p /etc/nginx/conf.d
+
 for conf in /etc/nginx/templates.d/*.conf; do
-    envsubst "$vars" < "$conf" | tee "/etc/nginx/http.d/$(basename $conf)"
+    envsubst "$vars" < "$conf" | tee "/etc/nginx/conf.d/$(basename $conf)"
 done
